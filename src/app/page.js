3bf6eae6144ad4c23824ae76ac4dbc21d1c6ef3b
hanidev08 <hanidev08@gmail.com>
2024-09-09 19:07:50 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,46 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const container = useRef(null);
+  const stickyMask = useRef(null);
+  const initialMaskSize = 0.6;
+  const targetMaskSize = 30;
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const easing = 0.15;
+  let easedScrollProgress = 0;
+  useEffect(() => {
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    const maskSizeProgress = targetMaskSize * getScrollProgress();
+    stickyMask.current.style.webkitMaskSize =
+      (initialMaskSize + maskSizeProgress) * 100 + "%";
+    requestAnimationFrame(animate);
+  };
+
+  const getScrollProgress = () => {
+    const scrollProgress =
+      stickyMask.current.offsetTop /
+      (container.current.getBoundingClientRect().height - window.innerHeight);
+
+    const delta = scrollProgress - easedScrollProgress;
+
+    easedScrollProgress += delta * easing;
+
+    return easedScrollProgress;
+  };
+  return (
+    <main className={styles.main}>
+      <div ref={container} className={styles.container}>
+        <div ref={stickyMask} className={styles.stickyMask}>
+          <video autoPlay muted loop>
+            <source src="/medias/nature1.mp4" type="video/mp4" />
+          </video>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
